4fdbf9ae38262b68c31ebe3611ead850e5f80231
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-25 22:52:20 +0530

    Create a constant to represent Entity

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"time"
 )
 
+//	Number of frames to render per second
 const FPS = 60
+
+//	The character to represent a single living entity
+const ENTITY = "◆"
 
 func main() {
 	grid := NewGrid(80, 120, true)
 
 	for {
 		grid.evolve()
-		grid.render("O")
+		grid.render(ENTITY)
 		time.Sleep((1000 / FPS) * time.Millisecond)
 	}
 }
